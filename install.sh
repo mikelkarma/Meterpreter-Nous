de71a032a7079c1974e13f47ec0a7905ff0d6e81
Mikelkarma <9semn0me9@gmail.com>
2025-02-03 16:00:52 +0000

    Suporte pra servidores onions adiconados

--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@ echo "[x] Instalando apktool & java"
 sudo apt install apktool openjdk-17-jdk
 echo "[x] Realizando algumas tarefas"
 apktool d templates/app.apk
+apktool d templates/app-tor.apk
+rm -rf templates/app
+rm -rf templates/app-tor
 mv app templates
+mv app-tor templates
 echo "[x] Pronto!"
